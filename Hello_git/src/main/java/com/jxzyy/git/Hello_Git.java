c3cc4b4e27e19adde37d4d16340c163938b422e4
adZ <1586270278@qq.com>
2022-03-04 11:01:52 +0800

    主干添加

--- Hello_git/src/main/java/com/jxzyy/git/Hello_Git.java
+++ Hello_git/src/main/java/com/jxzyy/git/Hello_Git.java
@@ -6,5 +6,6 @@ public class Hello_Git {
         System.out.println("Hello_Git");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
